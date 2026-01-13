b82f7115d6ddef0b97e8cdb96e6d1104c9fc34d8
Josimar Junior <josimarjunior@Josimars-MacBook-Pro.local>
2026-01-12 22:57:44 -0300

    init ecs with ec2

---
+++ variables.tf
@@ -0,0 +1,36 @@
+### General Configs ###
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "project_name" {
+  type = string
+}
+
+### SSM VPC ###
+variable "ssm_vpc_id" {}
+variable "ssm_public_subnet_1" {}
+variable "ssm_public_subnet_2" {}
+variable "ssm_public_subnet_3" {}
+variable "ssm_private_subnet_1" {}
+variable "ssm_private_subnet_2" {}
+variable "ssm_private_subnet_3" {}
+
+### Balancer ###
+variable "load_balancer_internal" {}
+variable "load_balancer_type" {}
+
+### ECS General ###
+variable "node_ami" {}
+variable "node_instance_type" {}
+variable "node_volume_size" {}
+variable "node_volume_type" {}
+
+variable "cluster_on_demand_min_size" {}
+variable "cluster_on_demand_max_size" {}
+variable "cluster_on_demand_desired_size" {}
+
+variable "cluster_spots_min_size" {}
+variable "cluster_spots_max_size" {}
+variable "cluster_spots_desired_size" {}
